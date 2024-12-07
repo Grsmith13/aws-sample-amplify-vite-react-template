c0d769bd705496ea1f49a2c6c4531d8f973599bc
grsmith44 <grsmith44@tntech.edu>
2024-12-07 02:47:11 -0600

    hopefully this works now that the ygo.table

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,9 +7,9 @@ const schema = a.schema({
     })
     .authorization(allow => [allow.publicApiKey()]),
 
-    // YGO_table: a.customType({
-    //   CardID: a.id().required(),
-    // })
+    YGO_table: a.customType({
+      CardID: a.id().required(),
+    })
 });
 
 export type Schema = ClientSchema<typeof schema>;
