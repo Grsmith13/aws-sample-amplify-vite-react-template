bd8969a0d3d688c35aff27489c0ea742bf0ca959
grsmith44 <grsmith44@tntech.edu>
2024-12-07 01:59:02 -0600

    added YGO_table custom type

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,30 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
 const client = generateClient<Schema>();
 
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+export default function App() {
+  const [todos, setTodos] = useState<Schema["Todo"]["type"][]>([]);
 
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
+  useEffect(() => {}, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  // const createTodo = async () => {
+  //   await client.models.Todo.create({
+  //     content: window.prompt("Todo content?"),
+  //     isDone: false,
+  //   });
+
+  // };
 
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
+    <div>
+      {/* <button onClick={createTodo}>Add new todo</button> */}
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+        {/* {todos.map(({ id, content }) => (
+          <li key={id}>{content}</li>
+        ))} */}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
-
-export default App;
